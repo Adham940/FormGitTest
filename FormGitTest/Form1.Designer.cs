150db28bb058d2730189d17c9baab5d7a641a997
Adham <adhmwaled94@gmail.com>
2025-04-22 17:39:25 +0200

    Add Lbl

--- FormGitTest/Form1.Designer.cs
+++ FormGitTest/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             button2 = new Button();
             flowLayoutPanel1 = new FlowLayoutPanel();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button2
@@ -52,11 +53,21 @@
             flowLayoutPanel1.Size = new Size(739, 393);
             flowLayoutPanel1.TabIndex = 2;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(579, 412);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 17);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(739, 448);
+            Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(flowLayoutPanel1);
             Name = "Form1";
@@ -64,11 +75,13 @@
             Text = "Form1";
             KeyDown += Form1_KeyDown;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button2;
         private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
     }
 }
